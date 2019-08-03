5cf304b2ce7458bd94a1be00893a61929d81e565
戸塚孝高 <y047aka@totsukayoshitakanoMac-mini.local>
2019-08-03 22:43:23 +0900

    webgl/crate.elm

--- src/Main.elm
+++ src/Main.elm
@@ -5,8 +5,15 @@ import Browser.Events exposing (onAnimationFrameDelta)
 import Html exposing (Html)
 import Html.Attributes exposing (height, style, width)
 import Math.Matrix4 as Mat4 exposing (Mat4)
+import Math.Vector2 as Vec2 exposing (Vec2, vec2)
 import Math.Vector3 as Vec3 exposing (Vec3, vec3)
+import Result
+import Task
 import WebGL
+import WebGL.Settings.Blend as Blend
+import WebGL.Settings.DepthTest as DepthTest
+import WebGL.Settings.StencilTest as StencilTest
+import WebGL.Texture as Texture exposing (Texture)
 
 
 
@@ -28,12 +35,18 @@ main =
 
 
 type alias Model =
-    Float
+    { texture : Maybe Texture.Texture
+    , theta : Float
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( 0, Cmd.none )
+    ( { texture = Nothing
+      , theta = 0
+      }
+    , Task.attempt TextureLoaded (Texture.load "https://elm-lang.org/assets/wood-crate.jpg")
+    )
 
 
 
@@ -41,14 +54,18 @@ init () =
 
 
 type Msg
-    = TimeDelta Float
+    = TextureLoaded (Result Texture.Error Texture.Texture)
+    | Animate Float
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg currentTime =
+update msg model =
     case msg of
-        TimeDelta elapsed ->
-            ( elapsed + currentTime, Cmd.none )
+        TextureLoaded result ->
+            ( { model | texture = Result.toMaybe result }, Cmd.none )
+
+        Animate dt ->
+            ( { model | theta = model.theta + dt / 10000 }, Cmd.none )
 
 
 
@@ -57,33 +74,100 @@ update msg currentTime =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    onAnimationFrameDelta TimeDelta
+    onAnimationFrameDelta Animate
 
 
 
 -- VIEW
 
 
-view : Float -> Html msg
-view t =
-    WebGL.toHtml
+view : Model -> Html msg
+view { texture, theta } =
+    WebGL.toHtmlWith
+        [ WebGL.alpha True
+        , WebGL.antialias
+        , WebGL.depth 1
+        , WebGL.stencil 0
+        ]
         [ width 400
         , height 400
         , style "display" "block"
         ]
-        [ WebGL.entity
-            vertexShader
-            fragmentShader
-            mesh
-            { perspective = perspective (t / 1000) }
-        ]
+        (texture
+            |> Maybe.map (scene (perspective theta))
+            |> Maybe.withDefault []
+        )
 
 
 perspective : Float -> Mat4
-perspective t =
-    Mat4.mul
-        (Mat4.makePerspective 45 1 0.01 100)
-        (Mat4.makeLookAt (vec3 (4 * cos t) 0 (4 * sin t)) (vec3 0 0 0) (vec3 0 1 0))
+perspective angle =
+    List.foldr Mat4.mul
+        Mat4.identity
+        [ Mat4.makePerspective 45 1 0.01 100
+        , Mat4.makeLookAt (vec3 0 3 8) (vec3 0 0 0) (vec3 0 1 0)
+        , Mat4.makeRotate (3 * angle) (vec3 0 1 0)
+        ]
+
+
+scene : Mat4 -> Texture -> List WebGL.Entity
+scene camera texture =
+    [ WebGL.entity
+        crateVertex
+        crateFragment
+        crateMesh
+        { texture = texture
+        , perspective = camera
+        }
+    , WebGL.entityWith
+        [ DepthTest.less
+            { write = False
+            , near = 0
+            , far = 1
+            }
+        , StencilTest.test
+            { ref = 1
+            , mask = 0xFF
+            , test = StencilTest.always
+            , fail = StencilTest.keep
+            , zfail = StencilTest.keep
+            , zpass = StencilTest.replace
+            , writeMask = 0xFF
+            }
+        ]
+        floorVertex
+        floorFragment
+        floorMesh
+        { texture = texture
+        , perspective = camera
+        }
+    , WebGL.entityWith
+        [ StencilTest.test
+            { ref = 1
+            , mask = 0xFF
+            , test = StencilTest.equal
+            , fail = StencilTest.keep
+            , zfail = StencilTest.keep
+            , zpass = StencilTest.keep
+            , writeMask = 0
+            }
+        , DepthTest.default
+        , Blend.custom
+            { r = 0
+            , g = 0
+            , b = 0
+            , a = 0.5
+            , color = Blend.customAdd Blend.constantAlpha Blend.zero
+            , alpha = Blend.customAdd Blend.one Blend.zero
+            }
+        ]
+        crateVertex
+        crateFragment
+        crateMesh
+        { texture = texture
+        , perspective =
+            Mat4.mul camera (Mat4.makeScale (vec3 1 -1 1))
+        }
+    ]
 
 
 
@@ -92,17 +176,81 @@ perspective t =
 
 type alias Vertex =
     { position : Vec3
-    , color : Vec3
+    , coord : Vec2
     }
 
 
-mesh : WebGL.Mesh Vertex
-mesh =
+crateMesh : WebGL.Mesh Vertex
+crateMesh =
+    [ ( 0, 0 ), ( 90, 0 ), ( 180, 0 ), ( 270, 0 ), ( 0, 90 ), ( 0, 270 ) ]
+        |> List.concatMap rotatedFace
+        |> WebGL.triangles
+
+
+rotatedFace : ( Float, Float ) -> List ( Vertex, Vertex, Vertex )
+rotatedFace ( angleXZ, angleYZ ) =
+    let
+        transformMat =
+            List.foldr Mat4.mul
+                Mat4.identity
+                [ Mat4.makeTranslate (vec3 0 1 0)
+                , Mat4.makeRotate (degrees angleXZ) Vec3.j
+                , Mat4.makeRotate (degrees angleYZ) Vec3.i
+                , Mat4.makeTranslate (vec3 0 0 1)
+                ]
+
+        transform vertex =
+            { vertex
+                | position =
+                    Mat4.transform
+                        transformMat
+                        vertex.position
+            }
+
+        transformTriangle ( a, b, c ) =
+            ( transform a, transform b, transform c )
+    in
+    List.map transformTriangle square
+
+
+square : List ( Vertex, Vertex, Vertex )
+square =
+    let
+        topLeft =
+            { position = vec3 -1 1 0, coord = vec2 0 1 }
+
+        topRight =
+            { position = vec3 1 1 0, coord = vec2 1 1 }
+
+        bottomLeft =
+            { position = vec3 -1 -1 0, coord = vec2 0 0 }
+
+        bottomRight =
+            { position = vec3 1 -1 0, coord = vec2 1 0 }
+    in
+    [ ( topLeft, topRight, bottomLeft )
+    , ( bottomLeft, topRight, bottomRight )
+    ]
+
+
+floorMesh : WebGL.Mesh { position : Vec3 }
+floorMesh =
+    let
+        topLeft =
+            { position = vec3 -2 0 -2 }
+
+        topRight =
+            { position = vec3 2 0 -2 }
+
+        bottomLeft =
+            { position = vec3 -2 0 2 }
+
+        bottomRight =
+            { position = vec3 2 0 2 }
+    in
     WebGL.triangles
-        [ ( Vertex (vec3 0 0 0) (vec3 1 0 0)
-          , Vertex (vec3 1 1 0) (vec3 0 1 0)
-          , Vertex (vec3 1 -1 0) (vec3 0 0 1)
-          )
+        [ ( topLeft, topRight, bottomLeft )
+        , ( bottomLeft, topRight, bottomRight )
         ]
 
 
@@ -111,29 +259,53 @@ mesh =
 
 
 type alias Uniforms =
-    { perspective : Mat4 }
+    { perspective : Mat4
+    , texture : Texture
+    }
 
 
-vertexShader : WebGL.Shader Vertex Uniforms { vcolor : Vec3 }
-vertexShader =
+crateVertex : WebGL.Shader Vertex Uniforms { vcoord : Vec2 }
+crateVertex =
     [glsl|
         attribute vec3 position;
-        attribute vec3 color;
+        attribute vec2 coord;
         uniform mat4 perspective;
-        varying vec3 vcolor;
+        varying vec2 vcoord;
         void main () {
-            gl_Position = perspective * vec4(position, 1.0);
-            vcolor = color;
+          gl_Position = perspective * vec4(position, 1.0);
+          vcoord = coord;
         }
     |]
 
 
-fragmentShader : WebGL.Shader {} Uniforms { vcolor : Vec3 }
-fragmentShader =
+crateFragment : WebGL.Shader {} { u | texture : Texture } { vcoord : Vec2 }
+crateFragment =
     [glsl|
         precision mediump float;
-        varying vec3 vcolor;
+        uniform sampler2D texture;
+        varying vec2 vcoord;
         void main () {
-            gl_FragColor = vec4(vcolor, 1.0);
+          gl_FragColor = texture2D(texture, vcoord);
+        }
+    |]
+
+
+floorVertex : WebGL.Shader { position : Vec3 } Uniforms {}
+floorVertex =
+    [glsl|
+        attribute vec3 position;
+        uniform mat4 perspective;
+        void main () {
+          gl_Position = perspective * vec4(position, 1.0);
+        }
+    |]
+
+
+floorFragment : WebGL.Shader attributes Uniforms {}
+floorFragment =
+    [glsl|
+        precision mediump float;
+        void main () {
+          gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
         }
     |]
